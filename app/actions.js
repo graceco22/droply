711f5a6e63b17c221dbb38ee3864d22c2f942533
graceco22 <graceco921@gmail.com>
2026-04-17 23:05:01 -0700

    Logic for add product

--- app/actions.js
+++ app/actions.js
@@ -10,3 +10,86 @@ export async function signOut() {
   revalidatePath("/");
   redirect("/");
 }
+
+export async function addProduct(formData) {
+  const url = formData.get("url");
+
+  if (!url) {
+    return { error: "URL is required" };
+  }
+
+  try {
+    const supabase = await createClient();
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+
+    if (!user) {
+      return { error: "Not authenticated" };
+    }
+
+    const productData = await scrapeProduct(url);
+
+    if (!productData.productName || !productData.currentPrice) {
+      console.log(productData, "productData");
+      return { error: "Could not extract product information from this URL" };
+    }
+
+    const newPrice = parseFloat(productData.currentPrice);
+    const currency = productData.currencyCode || "CAD";
+
+    const { data: existingProduct } = await supabase
+      .from("products")
+      .select("id, current_price")
+      .eq("user_id", user.id)
+      .eq("url", url)
+      .single();
+
+    const isUpdate = !!existingProduct;
+
+    const { data: product, error } = await supabase
+      .from("products")
+      .upsert(
+        {
+          user_id: user.id,
+          url,
+          name: productData.productName,
+          current_price: newPrice,
+          currency: currency,
+          image_url: productData.productImageUrl,
+          updated_at: new Date().toISOString(),
+        },
+        {
+          onConflict: "user_id,url",
+          ignoreDuplicates: false,
+        },
+      )
+      .select()
+      .single();
+
+    if (error) throw error;
+
+    const shouldAddHistory =
+      !isUpdate || existingProduct.current_price !== newPrice;
+
+    if (shouldAddHistory) {
+      await supabase.from("price_history").insert({
+        product_id: product.id,
+        price: newPrice,
+        currency: currency,
+      });
+    }
+
+    revalidatePath("/");
+    return {
+      success: true,
+      product,
+      message: isUpdate
+        ? "Product updated with latest price!"
+        : " Product added successfully!",
+    };
+  } catch (error) {
+    console.error("Add product error:", error);
+    return { error: error.message || "Failed to add product" };
+  }
+}
