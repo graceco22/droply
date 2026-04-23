a80b38c836aac923f8b601ea0ec55b77cc26a1c2
graceco22 <graceco921@gmail.com>
2026-04-22 19:21:40 -0700

    Initial setup for recharts on price chart

--- components/PriceChart.jsx
+++ components/PriceChart.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 const PriceChart = () => {
-  return (
-    <div>PriceChart</div>
-  )
-}
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-export default PriceChart
+  return (
+    <LineChart
+      style={{ width: "100%", aspectRatio: 1.618, maxWidth: 600 }}
+      responsive
+      data={data}
+      margin={{
+        top: 20,
+        right: 20,
+        bottom: 5,
+        left: 0,
+      }}>
+      <CartesianGrid stroke="#aaa" strokeDasharray="5 5" />
+      <Line
+        type="monotone"
+        dataKey="uv"
+        stroke="purple"
+        strokeWidth={2}
+        name="My data series name"
+      />
+      <XAxis dataKey="name" tick={renderCustomAxisTick} height={50} />
+      <YAxis
+        width="auto"
+        label={{ value: "UV", position: "insideLeft", angle: -90 }}
+      />
+      <Legend align="right" />
+      <Tooltip />
+    </LineChart>
+  );
+};
+
+export default PriceChart;
